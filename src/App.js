5834b7d6a24f1b0266f9bdfc4b67b04e586d89f8
adamekberg <ekberg.adam@hotmail.com>
2019-02-02 16:14:50 +0100

    added checkbox

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import DeckGL, { LineLayer, GeoJsonLayer } from 'deck.gl';
 import { StaticMap } from 'react-map-gl';
 import geoData from './data/Stockholm_Parking.json';
+import { Checkbox } from 'semantic-ui-react'
 
 // Set your mapbox access token here
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYWRhbWVrYmVyZyIsImEiOiJjamttcXdjeDMwZHd0M2tvemx1a3BnZ2h5In0.JtC9rUXVaxJ8ONGdfmPmsg';
@@ -17,43 +18,76 @@ const initialViewState = {
   bearing: 0
 };
 
-// Data to be used by the LineLayer
-const data = [
-  {
-    sourcePosition: [18.0686, 59.3293],
-    targetPosition: [20.0686, 61.3293]
+const parkingLayer = new GeoJsonLayer({
+  id: 'geojson-layer',
+  data: geoData,
+  pickable: true,
+  stroked: true,
+  filled: true,
+  extruded: true,
+  lineWidthScale: 20,
+  lineWidthMinPixels: 2,
+  getFillColor: [160, 160, 180, 200],
+  getLineColor: [0, 0, 0, 255],
+  getRadius: 100,
+  getLineWidth: 1,
+  getElevation: 30,
+  visible: false,
+  onHover: ({object, x, y}) => {
+  //console.log('h', object)
+  //   const tooltip = object.properties.name || object.properties.station;
+  //    Update tooltip
+  //      http://deck.gl/#/documentation/developer-guide/adding-interactivity?section=example-display-a-tooltip-for-hovered-object
+
   }
-];
+})
+
+
 
 class App extends React.Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      showParking: true
+    };
+  }
+
+  createParkingLayer() {
+    const showIt = this.state.showParking;
+
+    return new GeoJsonLayer({
+      id: 'geojson-layer',
+      data: geoData,
+      pickable: true,
+      stroked: true,
+      filled: true,
+      extruded: true,
+      lineWidthScale: 20,
+      lineWidthMinPixels: 2,
+      getFillColor: [160, 160, 180, 200],
+      getLineColor: [0, 0, 0, 255],
+      getRadius: 100,
+      getLineWidth: 1,
+      getElevation: 30,
+      visible: showIt,
+      onHover: ({object, x, y}) => {
+      //console.log('h', object)
+      //   const tooltip = object.properties.name || object.properties.station;
+      //    Update tooltip
+      //      http://deck.gl/#/documentation/developer-guide/adding-interactivity?section=example-display-a-tooltip-for-hovered-object
+    
+      }
+    })
+  }
+
+  
+
   render() {
-    const layers = [
-      new LineLayer({id: 'line-layer', data}),
-      new GeoJsonLayer({
-        id: 'geojson-layer',
-        data: geoData,
-        pickable: true,
-        stroked: true,
-        filled: true,
-        extruded: true,
-        lineWidthScale: 20,
-        lineWidthMinPixels: 2,
-        getFillColor: [160, 160, 180, 200],
-        getLineColor: [0, 0, 0, 255],
-        getRadius: 100,
-        getLineWidth: 1,
-        getElevation: 30,
-        onHover: ({object, x, y}) => {
-          console.log('h', object)
-        //   const tooltip = object.properties.name || object.properties.station;
-        //    Update tooltip
-        //      http://deck.gl/#/documentation/developer-guide/adding-interactivity?section=example-display-a-tooltip-for-hovered-object
-
-        }
-      })
-    ];
-
+    console.log('hej')
+    const layers = [this.createParkingLayer()];
+    
     return (
       <div>
 
@@ -64,12 +98,24 @@ class App extends React.Component {
       >
         <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
       </DeckGL>
-
-      <div style={{'zIndex':10000,'position':'absolute'}}>ABCDEFGHI!!!</div>
-
+      <div style={{'zIndex':10000,'position':'absolute'}}>
+        <input ref="parkingCheck" type="checkbox" defaultChecked="true" className="hidden" readOnly="" tabIndex="0" onChange={ () => this.handleChecked() }/>
+        <label>Show stuff</label>
+      </div>
+      
       </div>
     );
   }
+
+
+
+
+
+  handleChecked() {
+    console.log(this.refs.parkingCheck.checked)
+    this.setState({showParking: this.refs.parkingCheck.checked })
+  }
+
 }
 
 export default App;
