130da5d177a7450ba3fa68abbeb425b9742d5716
adamekberg <ekberg.adam@hotmail.com>
2019-02-23 19:59:33 +0100

    New formatting using prettier

--- src/Components/Controller.js
+++ src/Components/Controller.js
@@ -1,17 +1,15 @@
-import React from 'react';
+import React from "react";
 import {
   parkingLayer,
   busStopLayer,
   cycleTrafficLayer,
   carTrafficLayer
-} from '../layers/layers'
-import '../Controller.css'
+} from "../layers/layers";
+import "../Controller.css";
 
-
-import { Checkbox } from 'semantic-ui-react';
+import { Checkbox } from "semantic-ui-react";
 
 class App extends React.Component {
-
   constructor() {
     super();
 
@@ -21,102 +19,152 @@ class App extends React.Component {
       showCycleTraffic: false,
       extrudeCycleTraffic: true,
       showCarTraffic: true,
-      extrudeCarTraffic: true,
+      extrudeCarTraffic: true
     };
   }
 
   _toggleParking = (e, { checked }) => {
-    this.setState({showParking: checked })
+    this.setState({ showParking: checked });
     // Note: Dirty - find the right way to do this
-    setTimeout(this._updateLayers,0)
-  }
+    setTimeout(this._updateLayers, 0);
+  };
 
   _toggleBusStops = (e, { checked }) => {
-    this.setState({showBusStops: checked })
+    this.setState({ showBusStops: checked });
     // Note: Dirty - find the right way to do this
-    setTimeout(this._updateLayers,0)
-  }
+    setTimeout(this._updateLayers, 0);
+  };
 
   _toggleCycleTraffic = (e, { checked }) => {
-    this.setState({showCycleTraffic: checked })
+    this.setState({ showCycleTraffic: checked });
     // Note: Dirty - find the right way to do this
-    setTimeout(this._updateLayers,0)
-  }
+    setTimeout(this._updateLayers, 0);
+  };
 
   _toggleCycleTrafficExtrude = (e, { checked }) => {
-    this.setState({extrudeCycleTraffic: checked })
+    this.setState({ extrudeCycleTraffic: checked });
     // Note: Dirty - find the right way to do this
-    setTimeout(this._updateLayers,0)
-  }
+    setTimeout(this._updateLayers, 0);
+  };
 
   _toggleCarTraffic = (e, { checked }) => {
-    this.setState({showCarTraffic: checked })
+    this.setState({ showCarTraffic: checked });
     // Note: Dirty - find the right way to do this
-    setTimeout(this._updateLayers,0)
-  }
+    setTimeout(this._updateLayers, 0);
+  };
 
   _toggleCarTrafficExtrude = (e, { checked }) => {
-    this.setState({extrudeCarTraffic: checked })
+    this.setState({ extrudeCarTraffic: checked });
     // Note: Dirty - find the right way to do this
-    setTimeout(this._updateLayers,0)
-  }
+    setTimeout(this._updateLayers, 0);
+  };
 
   _updateLayers = () => {
-
     let layers = [
-        parkingLayer( this.state.showParking ),
-        busStopLayer( this.state.showBusStops ),
-        cycleTrafficLayer( this.state.showCycleTraffic, this.state.extrudeCycleTraffic ),
-        carTrafficLayer( this.state.showCarTraffic, this.state.extrudeCarTraffic )
-      ]
+      parkingLayer(this.state.showParking),
+      busStopLayer(this.state.showBusStops),
+      cycleTrafficLayer(
+        this.state.showCycleTraffic,
+        this.state.extrudeCycleTraffic
+      ),
+      carTrafficLayer(this.state.showCarTraffic, this.state.extrudeCarTraffic)
+    ];
 
-    this.props.onLayerChange( layers )
-
-  }
+    this.props.onLayerChange(layers);
+  };
 
   componentDidMount() {
-    this._updateLayers()
+    this._updateLayers();
   }
 
   render() {
-    return(
+    return (
       <div className="controller-container">
         <h3>Stockholm Sustainable Traffic Planning</h3>
         <p>Toggle the checkboxes below to turn layers on&nbsp;and&nbsp;off</p>
 
         <div className="controller-option option-parking">
-          <Checkbox label="Parking" ref="parkingCheck" type="checkbox" onChange={ this._toggleParking }/>
+          <Checkbox
+            label="Parking"
+            ref="parkingCheck"
+            type="checkbox"
+            onChange={this._toggleParking}
+          />
         </div>
         <div className="controller-option option-bus-stops">
-          <Checkbox label="Bus Stops" ref="busStopCheck" type="checkbox" defaultChecked onChange={ this._toggleBusStops }/>
+          <Checkbox
+            label="Bus Stops"
+            ref="busStopCheck"
+            type="checkbox"
+            defaultChecked
+            onChange={this._toggleBusStops}
+          />
         </div>
         <div className="controller-option option-cycle-traffic">
-          <Checkbox label="Bicycle Traffic" ref="cycleTrafficCheck" type="checkbox" onChange={ this._toggleCycleTraffic }/>
+          <Checkbox
+            label="Bicycle Traffic"
+            ref="cycleTrafficCheck"
+            type="checkbox"
+            onChange={this._toggleCycleTraffic}
+          />
         </div>
         <div className="controller-option option-extrude-checkbox">
-        {
-          this.state.showCycleTraffic ?
-          <Checkbox label="Extrude Bicycle Traffic" ref="cycleTrafficCheck" type="checkbox" defaultChecked onChange={ this._toggleCycleTrafficExtrude }/> :
-          <Checkbox label="Extrude Bicycle Traffic" ref="cycleTrafficCheck" type="checkbox" defaultChecked disabled onChange={ this._toggleCycleTrafficExtrude }/>
-        }
+          {this.state.showCycleTraffic ? (
+            <Checkbox
+              label="Extrude Bicycle Traffic"
+              ref="cycleTrafficCheck"
+              type="checkbox"
+              defaultChecked
+              onChange={this._toggleCycleTrafficExtrude}
+            />
+          ) : (
+            <Checkbox
+              label="Extrude Bicycle Traffic"
+              ref="cycleTrafficCheck"
+              type="checkbox"
+              defaultChecked
+              disabled
+              onChange={this._toggleCycleTrafficExtrude}
+            />
+          )}
         </div>
 
         <div className="controller-option option-car-traffic">
-          <Checkbox label="Vehicle Traffic" ref="carTrafficCheck" type="checkbox" defaultChecked onChange={ this._toggleCarTraffic }/>
+          <Checkbox
+            label="Vehicle Traffic"
+            ref="carTrafficCheck"
+            type="checkbox"
+            defaultChecked
+            onChange={this._toggleCarTraffic}
+          />
         </div>
         <div className="controller-option option-extrude-checkbox">
-        {
-          this.state.showCarTraffic ?
-          <Checkbox label="Extrude Vehicle Traffic" ref="carTrafficCheck" type="checkbox" defaultChecked onChange={ this._toggleCarTrafficExtrude }/> :
-          <Checkbox label="Extrude Vehicle Traffic" ref="carTrafficCheck" type="checkbox" defaultChecked disabled onChange={ this._toggleCarTrafficExtrude }/>
-        }
+          {this.state.showCarTraffic ? (
+            <Checkbox
+              label="Extrude Vehicle Traffic"
+              ref="carTrafficCheck"
+              type="checkbox"
+              defaultChecked
+              onChange={this._toggleCarTrafficExtrude}
+            />
+          ) : (
+            <Checkbox
+              label="Extrude Vehicle Traffic"
+              ref="carTrafficCheck"
+              type="checkbox"
+              defaultChecked
+              disabled
+              onChange={this._toggleCarTrafficExtrude}
+            />
+          )}
         </div>
 
-        <div className="controller-option">*right click and drag to rotate camera</div>
+        <div className="controller-option">
+          *right click and drag to rotate camera
+        </div>
       </div>
-    )
+    );
   }
-
 }
 
 export default App;
--- src/layers/layers.js
+++ src/layers/layers.js
@@ -1,9 +1,9 @@
-import { GeoJsonLayer, HexagonLayer } from 'deck.gl';
+import { GeoJsonLayer, HexagonLayer } from "deck.gl";
 
-import parkingData from '../data/Stockholm_Parking.json';
-import busStopData from '../data/bus_stops_geo.json';
-import cycleTrafficData from '../data/cycle_data_final.json';
-import carTrafficData from '../data/cars_data_final.json';
+import parkingData from "../data/Stockholm_Parking.json";
+import busStopData from "../data/bus_stops_geo.json";
+import cycleTrafficData from "../data/cycle_data_final.json";
+import carTrafficData from "../data/cars_data_final.json";
 
 const colorRange = [
   [1, 152, 189],
@@ -23,9 +23,9 @@ const LIGHT_SETTINGS = {
   numberOfLights: 2
 };
 
-const parkingLayer = (show=true) => {
+const parkingLayer = (show = true) => {
   return new GeoJsonLayer({
-    id: 'parking-layer',
+    id: "parking-layer",
     data: parkingData,
     pickable: true,
     stroked: true,
@@ -38,7 +38,7 @@ const parkingLayer = (show=true) => {
     getRadius: 100,
     getLineWidth: 1,
     getElevation: 0,
-    visible: show,
+    visible: show
     // onHover: ({object, x, y}) => {
     //console.log('h', object)
     //   const tooltip = object.properties.name || object.properties.station;
@@ -46,13 +46,12 @@ const parkingLayer = (show=true) => {
     //      http://deck.gl/#/documentation/developer-guide/adding-interactivity?section=example-display-a-tooltip-for-hovered-object
 
     // }
-  })
-}
+  });
+};
 
-const busStopLayer = (show=true) => {
-
+const busStopLayer = (show = true) => {
   return new GeoJsonLayer({
-    id: 'bus-stop-layer',
+    id: "bus-stop-layer",
     data: busStopData,
     pickable: true,
     stroked: true,
@@ -65,7 +64,7 @@ const busStopLayer = (show=true) => {
     getRadius: 10,
     getLineWidth: 1,
     getElevation: 0,
-    visible: show,
+    visible: show
     // onHover: ({object, x, y}) => {
     // //console.log('h', object)
     //   if(object) {
@@ -75,18 +74,16 @@ const busStopLayer = (show=true) => {
     //     // this.props.onHover(tooltip)
     //   }
     // }
-  })
+  });
+};
 
-}
-
-const cycleTrafficLayer = (show=true, extruded=true) => {
-
+const cycleTrafficLayer = (show = true, extruded = true) => {
   function getColorValue(points) {
-    return points.reduce((a,c) => a + c.value, 0)
+    return points.reduce((a, c) => a + c.value, 0);
   }
 
   return new HexagonLayer({
-    id: 'hexagon-layer',
+    id: "hexagon-layer",
     data: cycleTrafficData,
     pickable: true,
     extruded: extruded,
@@ -94,79 +91,80 @@ const cycleTrafficLayer = (show=true, extruded=true) => {
     elevationScale: 1,
     colorRange,
     getPosition: d => {
-      return [ +d.long, +d.lat ]
+      return [+d.long, +d.lat];
     },
     getColorValue,
     getElevationValue: getColorValue,
     lightSettings: LIGHT_SETTINGS,
-    opacity:1,
-    visible: show,
+    opacity: 1,
+    visible: show
     // onHover: ({object, x, y}) => {
-      // const tooltip = `${object.centroid.join(', ')}\nCount: ${object.points.length}`;
-      /* Update tooltip
+    // const tooltip = `${object.centroid.join(', ')}\nCount: ${object.points.length}`;
+    /* Update tooltip
          http://deck.gl/#/documentation/developer-guide/adding-interactivity?section=example-display-a-tooltip-for-hovered-object
       */
     // }
-  })
-}
+  });
+};
 
-const carTrafficLayer = (show=true, extruded=true) => {
-
+const carTrafficLayer = (show = true, extruded = true) => {
   function getColorValue(points) {
-    return points.reduce((a,c) => a + +c.value, 0)
+    return points.reduce((a, c) => a + +c.value, 0);
   }
-  
-    return new HexagonLayer({
-      id: 'car-traffic-layer',
-      data: carTrafficData,
-      pickable: true,
-      extruded: extruded,
-      radius: 20,
-      elevationScale: 1,
-      colorRange,
-      getPosition: d => {
-        return [ +d.long, +d.lat ]
-      },
-      getColorValue,
-      getElevationValue: getColorValue,
-      visible: true,
-      lightSettings: LIGHT_SETTINGS,
-      opacity:1,
-      visible: show
-    })
-  }
+
+  return new HexagonLayer({
+    id: "car-traffic-layer",
+    data: carTrafficData,
+    pickable: true,
+    extruded: extruded,
+    radius: 20,
+    elevationScale: 1,
+    colorRange,
+    getPosition: d => {
+      return [+d.long, +d.lat];
+    },
+    getColorValue,
+    getElevationValue: getColorValue,
+    visible: true,
+    lightSettings: LIGHT_SETTINGS,
+    opacity: 1,
+    visible: show
+  });
+};
 
 const buildingsLayer = () => {
-
   return {
-    'id': '3d-buildings',
-    'source': 'composite',
-    'source-layer': 'building',
-    'filter': ['==', 'extrude', 'true'],
-    'type': 'fill-extrusion',
-    'minzoom': 15,
-    'paint': {
-      'fill-extrusion-color': '#aaa',
+    id: "3d-buildings",
+    source: "composite",
+    "source-layer": "building",
+    filter: ["==", "extrude", "true"],
+    type: "fill-extrusion",
+    minzoom: 15,
+    paint: {
+      "fill-extrusion-color": "#aaa",
       // use an 'interpolate' expression to add a smooth transition effect to the
       // buildings as the user zooms in
-      'fill-extrusion-height': [
-        "interpolate", ["linear"], ["zoom"],
-        15, 0,
-        15.05, ["get", "height"]
-        ],
-        'fill-extrusion-base': [
-        "interpolate", ["linear"], ["zoom"],
-        15, 0,
-        15.05, ["get", "min_height"]
-        ],
-        'fill-extrusion-opacity': .6
+      "fill-extrusion-height": [
+        "interpolate",
+        ["linear"],
+        ["zoom"],
+        15,
+        0,
+        15.05,
+        ["get", "height"]
+      ],
+      "fill-extrusion-base": [
+        "interpolate",
+        ["linear"],
+        ["zoom"],
+        15,
+        0,
+        15.05,
+        ["get", "min_height"]
+      ],
+      "fill-extrusion-opacity": 0.6
     }
-  }
-}
+  };
+};
 
-export {
-  parkingLayer,
-  busStopLayer,
-  cycleTrafficLayer,
-  carTrafficLayer
-}
+export { parkingLayer, busStopLayer, cycleTrafficLayer, carTrafficLayer };
