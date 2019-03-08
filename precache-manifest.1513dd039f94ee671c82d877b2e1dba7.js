1aeaeb40b3dc555b25e5cc532b722dc9f91cf517
thetylerwolf <tylernicholaswolf@gmail.com>
2019-03-08 11:52:28 +0100

    Updates

---
+++ precache-manifest.1513dd039f94ee671c82d877b2e1dba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "17469212b7922cd844d0",
+    "url": "/govchallenge/static/js/runtime~main.17469212.js"
+  },
+  {
+    "revision": "70decf7e1509f3543205",
+    "url": "/govchallenge/static/js/main.70decf7e.chunk.js"
+  },
+  {
+    "revision": "0e8798b6f8afdd9d9b2c",
+    "url": "/govchallenge/static/js/1.0e8798b6.chunk.js"
+  },
+  {
+    "revision": "70decf7e1509f3543205",
+    "url": "/govchallenge/static/css/main.da9807da.chunk.css"
+  },
+  {
+    "revision": "0e8798b6f8afdd9d9b2c",
+    "url": "/govchallenge/static/css/1.d618bada.chunk.css"
+  },
+  {
+    "revision": "97b5cfe033fb3767a055e123768f44dc",
+    "url": "/govchallenge/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/govchallenge/precache-manifest.8eeeaf55e267fc7b016e530c1aef4994.js"
+  "/govchallenge/precache-manifest.1513dd039f94ee671c82d877b2e1dba7.js"
 );
 
 workbox.clientsClaim();
